4b4879714c1f2f8d6b8e36a6bdcc9af933275c4b
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-04-04 15:46:44 +0900

    배포 준비
    배포 준비
    helmet, hpp, pm2, cross-env 설치

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path')
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv')
+const helmet = require('helmet');
+const hpp = require('hpp');
 
 dotenv.config();
 const indexRouter = require('./routes');
-const webSokcet = require('./socket');
+//const webSokcet = require('./socket');
 
 const app = express();
 app.set('port', process.env.PORT || 8000);
@@ -17,24 +18,18 @@ nunjucks.configure('views', {
     express: app,
     watch: true
 });
-    
-const sessionMiddleware = session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    }
-});
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined'));
+    app.use(helmet({contentSecurityPolicy: false}));
+    app.use(hpp());
+} else {
+    app.use(morgan('dev'));
+}
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(sessionMiddleware);
-
 
 app.use('/', indexRouter);
 
@@ -51,8 +46,6 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-const server = app.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 })
-
-webSokcet(server, app, sessionMiddleware);
